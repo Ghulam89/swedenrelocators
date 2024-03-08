7f3abaf2cd0f80703be98e21de0d5a1b1cf4beb6
Ghulam89 <gm414157@gmail.com>
2024-03-08 04:40:56 -0800

    update

--- src/components/Common/Banner_Page.jsx
+++ src/components/Common/Banner_Page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const Banner_Page = ({ title }) => {
   return (
     <>
-      <div className={" tw-h-[40vh]   tw-absolute tw-top-20 -tw-z-10 tw-bg-[#4D5160] tw-w-full tw-flex tw-justify-center tw-items-end"}>
+      <div className={" md:tw-h-[40vh] tw-h-[70vh]   tw-absolute tw-top-20 -tw-z-10 tw-bg-[#4D5160] tw-w-full tw-flex tw-justify-center tw-items-end"}>
         <div className=" tw-pb-7">
           <h1 className=" tw-font-bold text-white tw-uppercase">{title}</h1>
         </div>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,15 +6,15 @@ const Hero = () => {
     <>
       <section className=" tw-pb-16">
         <div className=" container">
-          <div className=" row tw-items-center g-5">
+          <div className=" row tw-items-center">
             <div className=" col-md-6">
               <h6 className=" tw-text-gray tw-uppercase tw-text-lg">Relocation solutions for investors & international employees</h6>
-              <h1 className=" tw-leading-[60px] sm:tw-text-5xl tw-text-3xl  tw-font-normal">
+              <h1 className=" tw-leading-[60px] tw-text-5xl  tw-font-normal">
                 Relocate To <strong className=" tw-text-blue">Sweden</strong>{" "} <br/>
                 With  an <strong className=" tw-text-blue">Expert Advise</strong>
               </h1>
               <p className=" tw-text-gray">We offer complete Relocation Services to our worldwide clients including, Residene & Work Permit,Home Finding, School-Pre Search, Integration, and Setting-in Services</p>
-              <div className="row  g-4">
+              <div className="row">
                 <div className=" col-md-4">
                   <Button className={"btn-warning"} label={"Our Services"} />
                 </div>
